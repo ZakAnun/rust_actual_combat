a7c31f3c3ef02a24a8276270a0336756bd1ce5a0
Zak <linhenji@163.com>
2024-07-16 22:20:16 +0800

    struct known.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user = User {
+        active: true,
+        username: String::from("realUserName"),
+        email: String::from("realUserName@163.com"),
+        sign_in_count: 1,
+    };
+
+    let mut mutUser = User {
+        active: true,
+        username: String::from("mutUserName"),
+        email: String::from("mutUserName@163.com"),
+        sign_in_count: 2,
+    };
+
+    mutUser.email = String::from("mutUserName1@163.com");
 }
